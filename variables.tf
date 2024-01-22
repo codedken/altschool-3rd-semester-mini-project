41fdafaf61b230b33e87a68fb84ae8275f9dac06
Chibuzor Ojukwu <cbsmoothoj@gmail.com>
2024-01-22 13:21:07 +0100

    added a variable name for reusability

--- variables.tf
+++ variables.tf
@@ -17,3 +17,23 @@ variable "project_name" {
 variable "key_pair" {
   default = "dev-key"
 }
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "subnet1_cidr" {
+  default = "10.0.0.0/24"
+}
+variable "subnet2_cidr" {
+  default = "10.0.32.0/24"
+}
+variable "subnet3_cidr" {
+  default = "10.0.64.0/24"
+}
+
+variable "domain_name" {
+  default     = "prayandcode.com.ng"
+  type        = string
+  description = "My domain name"
+}
