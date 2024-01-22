73a86d84618517abf53b4ab394ac0e4a30678303
Chibuzor Ojukwu <cbsmoothoj@gmail.com>
2024-01-22 13:13:16 +0100

    added a variable name for reusability

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "aws" {
 
 # Create a VPC
 resource "aws_vpc" "my_vpc" {
-  cidr_block           = "10.0.0.0/16"
+  cidr_block           = var.vpc_cidr
   enable_dns_hostnames = true
   enable_dns_support   = true
 
